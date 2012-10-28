fd3f2bc310ef3c8cace8450676470ee005fd3afa
gregmalcolm <greg@gregmalcolm.com>
2012-10-28 18:23:35 -0400

    Creating shapes

--- spec/model_spec.js
+++ spec/model_spec.js
@@ -1,15 +1,40 @@
-describe("Model", function() {
-    var subject = undefined;
+describe("tetrominoes.model", function() {
+    var subject;
     Given(function() { this.subject = tetrominoes.model; });
-    describe("#init", function() {
-        var view = undefined;
+    describe(".init", function() {
+        var view;
         Given(function() {
             this.view = { canvas: { width: 504, height: 612} }
         });
-        When(function() { this.subject.init(this.view); });
-        Then(function() { return this.subject.canvasWidth  === 504; });
-        Then(function() { return this.subject.canvasHeight === 612; });
-        Then(function() { return this.subject.blockWidth === 36; });
-        Then(function() { return this.subject.blockHeight === 36; });
+
+        describe("metrics", function() {
+            When(function() { this.subject.init(this.view); });
+            Then(function() { return this.subject.canvasWidth  === 504; });
+            Then(function() { return this.subject.canvasHeight === 612; });
+            Then(function() { return this.subject.blockWidth === 36; });
+            Then(function() { return this.subject.blockHeight === 36; });
+        });
+
+        describe("shapes", function() {
+            var shape;
+            When(function() { this.subject.init(this.view); });
+            Then(function() { return this.subject.shapes.length === 1 });
+
+            describe("#1", function() {
+                Given(function() { this.shape = this.subject.shapes[0] });
+//                Then(function() { return this.shape.positions[1].blocks[3].x === 2; } );
+//                Then(function() { return this.shape.positions[1].blocks[3].y === 0; } );
+            });
+        });
+    });
+});
+
+describe("model.Shape", function() {
+    describe(".spawn", function() {
+        context("with only one block", function() {
+            When(function() { this.shape = model.Shape.spawn({blocks: [["X"]]}) });
+            Then(function() { this.shape.positions[0].blocks[0].x === 0 });
+        });
+
     });
 });
--- src/model.js
+++ src/model.js
@@ -1,17 +1,74 @@
 var app = tetrominoes;
+var model = {};
 
 app.model = {
-    canvasWidth: undefined,
-    canvasHeight: undefined,
-    blockWidth: undefined,
-    blockHeight: undefined,
-    widthInBlocks: 14,
-    heightInBlocks: 17,
+    canvasWidth : undefined,
+    canvasHeight : undefined,
+    blockWidth : undefined,
+    blockHeight : undefined,
+    widthInBlocks : 14,
+    heightInBlocks : 17,
+    shapes : [],
 
     init: function(view) {
-        this.canvasWidth  = view.canvas.width;
+        this.initMetrics(view);
+        this.shapes = this.buildShapes();
+    },
+
+    initMetrics: function(view) {
+        this.canvasWidth = view.canvas.width;
         this.canvasHeight = view.canvas.height;
-        this.blockWidth  = this.canvasWidth  / this.widthInBlocks;
+        this.blockWidth = this.canvasWidth / this.widthInBlocks;
         this.blockHeight = this.canvasHeight / this.heightInBlocks;
-    }
+    },
+
+    buildShapes: function() {
+        var self = [];
+
+        self.push(model.Shape.spawn({blocks:
+            [["X",
+              "O",
+              "X",
+              "X"],
+
+             ["XOXX"]]})
+        );
+
+        return self;
+    },
+
+};
+
+model.Shape = {
+    positions: [],
+
+    spawn: function(args) {
+        var self = Object.create(model.Shape);
+
+        self.positions = this.calcPositions(args.blocks)
+
+        return self;
+    },
+
+    calcPositions: function(blocks) {
+        var positions = [];
+        var offsetX = 0;
+        var offsetY = 0;
+
+        for(var pos = 0; pos < blocks.length; ++pos) {
+            positions[pos] = { blocks: [] };
+            for(var y = 0; y < blocks[pos].length; ++y) {
+                block_num=0;
+                for(var x = 0; x < blocks[pos][y].length; ++x) {
+                    block = blocks[pos][y][x];
+                    if (block !== " ") {
+                        positions[pos].blocks.push( {'x' : x,
+                                                     'y' : y });
+                    }
+                }
+            }
+        }
+
+        return positions;
+    },
 };
